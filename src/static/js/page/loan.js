cd555935391fab0469e7d0f19ccdeb28d3076fca
LiuDongFeng <863258996@qq.com>
2017-01-03 11:08:23 +0800

    修改分页器回调函数名

--- src/static/js/page/loan.js
+++ src/static/js/page/loan.js
@@ -1,21 +1,27 @@
 'use strict';
 (function(g) {
-	var $scope = pageCtrl['loan'] = {};
+	var $scope = {};
+	var page = pageCtrl['loan'] = {};
 	var _ = g.render;
+	page.template = g.router.template('main');
 	$scope.loadLoanList = function() {
 		$.ajax({
 			url: $http.api('loan/list'),
 			success: $http.ok(function(data) {
-				_.compile(_.$console, g.router.template('main'), data, $scope.async);
+				_.compile(_.$console, page.template, data, $scope.async);
 			})
 		})
 	}	
 	$scope.async = function() {
-		console.log(1);
 		$('#pageToolbar').paging();
 	}
 	$scope.start = function() {
 		$scope.loadLoanList();
+	}
+	
+	page.paging = function(page, pageSize, $el, cb) {
+		console.log(arguments);
+		cb();
 	}
 
 	$scope.start();
