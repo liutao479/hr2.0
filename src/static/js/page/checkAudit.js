399a09a75e15a81228aa89d02a4cc641e32f3406
hyzou <2218112103@qq.com>
2017-03-07 15:45:24 +0800

    12

--- src/static/js/page/checkAudit.js
+++ src/static/js/page/checkAudit.js
@@ -7,25 +7,183 @@ page.ctrl('checkAudit', function($scope) {
 			page: $params.page || 1,
 			pageSize: 20
 		};
-	var urlStr1 = "http://192.168.0.134:8080";
-	var urlStr = "http://127.0.0.1:8083";
-//	var urlStr1 = "http://127.0.0.1:8083";
+	var urlStr = "http://192.168.0.123:8080";
 	var apiMap = {
-		"dealerName": urlStr+"/mock/sex",
-		"dealerId": urlStr+"/mock/busiSourceName",
-		"sex": urlStr+"/mock/sex",
-		"hprovince": urlStr+"/mock/province",
-		"hcity": urlStr+"/mock/city",
-		"hcounty": urlStr+"/mock/country",
-		"cprovince": urlStr+"/mock/province",
-		"ccity": urlStr+"/mock/city",
-		"ccounty": urlStr+"/mock/country",
-		"isSecond": urlStr+"/mock/busiSourceName"
-		}
-	var rendMap = {
-		"phoneAudit": "phoneAudit.js",
-		"loanInfo": "loanInfo.js",
-		}
+		"serviceType": urlStr+"/loanConfigure/getItem",//业务类型
+		"brand": urlStr+"/demandBank/selectBank",//经办银行
+		"busiSourceType": urlStr+"/loanConfigure/getItem",//业务来源类型
+		"busiArea": urlStr+"/area/get",//三级下拉省市县
+		"busiSourceName": urlStr+"/carshop/list",//业务来源方名称
+		"busiSourceNameSearch": urlStr+"/carshop/searchCarShop",//业务来源方名称模糊搜索
+		"onLicensePlace": urlStr+"/area/get",//三级下拉省市县
+		"busimode": urlStr+"/loanConfigure/getItem",//业务模式
+		"carName": urlStr+"/car/carBrandList",//三级车辆型号:车辆品牌
+		"carNameSearch":  urlStr+"/car/searchCars",//车辆型号模糊搜索
+		"repaymentTerm": urlStr+"/loanConfigure/getItem", //还款期限
+		"remitAccountNumber": urlStr+"/demandCarShopAccount/getAccountList" //打款账号
+	};
+	var dataMap = {
+		    "sex":[
+		        {
+		            "name":"男",
+		            "value":"0"
+		        },
+		        {
+		            "name":"女",
+		            "value":"1"
+		        }
+		    ],
+		    "isSecond":[
+		        {
+		            "name":"新车",
+		            "value":"0"
+		        },
+		        {
+		            "name":"二手车",
+		            "value":"1"
+		        }
+		    ],
+		    "licenseType":[
+		        {
+		            "name":"公牌",
+		            "value":"0"
+		        },
+		        {
+		            "name":"私牌",
+		            "value":"1"
+		        }
+		    ],
+		    "isFinanceLeaseVehicle":[
+		        {
+		            "name":"是融资租赁车",
+		            "value":"0"
+		        },
+		        {
+		            "name":"不是融资租赁",
+		            "value":"1"
+		        }
+		    ],
+		    "isOperationVehicle":[
+		        {
+		            "name":"运营车",
+		            "value":"0"
+		        },
+		        {
+		            "name":"非运营车",
+		            "value":"1"
+		        }
+		    ],
+		    "isInstallGps":[
+		        {
+		            "name":"是",
+		            "value":"0"
+		        },
+		        {
+		            "name":"否",
+		            "value":"1"
+		        }
+		    ],
+		    "isDiscount":[
+		        {
+		            "name":"贴息",
+		            "value":"0"
+		        },
+		        {
+		            "name":"不贴息",
+		            "value":"1"
+		        }
+		    ],
+		    "renewalMode":[
+		        {
+		            "name":"自行办理",
+		            "value":"0"
+		        },
+		        {
+		            "name":"单位承保",
+		            "value":"1"
+		        }
+		    ],
+		    "renewalModeList":[
+		        {
+		            "name":"自行办理",
+		            "value":"0"
+		        },
+		        {
+		            "name":"单位承保",
+		            "value":"1"
+		        }
+		    ],
+		    "isAdvanced":[
+		        {
+		            "name":"需要垫资",
+		            "value":"0"
+		        },
+		        {
+		            "name":"不需要垫资",
+		            "value":"1"
+		        }
+		    ],
+		    "maritalStatus":[
+		        {
+		            "name":"已婚",
+		            "value":"0"
+		        },
+		        {
+		            "name":"未婚",
+		            "value":"1"
+		        }
+		    ],
+		    "houseStatus":[
+		        {
+		            "name":"有商品房",
+		            "value":"0"
+		        },
+		        {
+		            "name":"有房",
+		            "value":"1"
+		        }
+		    ],
+		    "isEnterprise":[
+		        {
+		            "name":"是企业法人",
+		            "value":"0"
+		        },
+		        {
+		            "name":"不是企业法人",
+		            "value":"1"
+		        }
+		    ],
+		    "userRelationship":[
+		        {
+		            "name":"父母",
+		            "value":"0"
+		        },
+		        {
+		            "name":"子女",
+		            "value":"1"
+		        }
+		    ],
+		    "relationship":[
+		        {
+		            "name":"同事",
+		            "value":"0"
+		        },
+		        {
+		            "name":"朋友",
+		            "value":"1"
+		        }
+		    ]
+		};
+	var postUrl = {
+		"saveOrderInfo": urlStr+"/loanInfoInput/updLoanOrder",
+		"saveCarInfo": urlStr+"/loanInfoInput/updLoanUserCar",
+		"saveStageInfo": urlStr+"/loanInfoInput/updLoanUserStage",
+		"saveCommonInfo": urlStr+"/loanInfoInput/updLoanUser",
+		"saveEmergencyInfo": urlStr+"/loanInfoInput/updLoanEmergencyConact",
+		"saveloanPayCardInfo": urlStr+"/loanInfoInput/updLoanPayCard",
+		"saveFYXXInfo": urlStr+"/loanInfoInput/updLoanFee",
+		"saveQTXX": urlStr+"/loanInfoInput/updLoanIndividuation"
+	};
 	/**
 	* 设置面包屑
 	*/
@@ -47,21 +205,47 @@ page.ctrl('checkAudit', function($scope) {
 	* @params {function} cb 回调函数
 	*/
 	var loadLoanList = function(params, cb) {
+		var data={};
+		data['taskId']=80872;
 		$.ajax({
-			url: $http.api('phoneAudit'),
-			data: params,
+			url: urlStr+'/loanTelApproval/info',
+			data: data,
 			success: $http.ok(function(result) {
-//				$scope.result = result;
+				$scope.result = result;
 //				// 启动面包屑
 //				var _loanUser = $scope.result.data[0].loanUserCredits[0].userName;
 //				setupLocation(_loanUser);
-//				render.compile($scope.$el.$tbl, $scope.def.listTmpl, result.data, true);
-//				if(cb && typeof cb == 'function') {
-//					cb();
-//				}
+				render.compile($scope.$el.$tbl, $scope.def.listTmpl, result.cfgData, true);
+				if(cb && typeof cb == 'function') {
+					cb();
+				}
 //				loanFinishedSelect();
+				setupEvent();
 			})
 		})
+	}
+	var setupEvent = function() {
+		/**
+		* 绑定立即处理事件
+		*/
+		$console.find('.tab-bar a').on('click', function() {
+			var that = $(this);
+			var idx = that.data('idx');
+			var loanTasks = $scope.pageData[idx].loanTasks;
+			var taskObj = {};
+			for(var i = 0, len = loanTasks.length; i < len; i++) {
+				var obj = loanTasks[i];
+				taskObj[obj.category] = {
+					taskId: obj.id,
+					scene: obj.sceneName
+				}
+			}
+			router.render(that.data('href'), {
+				tasks: taskObj,
+				path: 'checkAudit'
+			});
+		});
+		// $('.select').dropdown($scope);
 	}
 //页面加载完成对所有下拉框进行赋值	
 	var loanFinishedSelect = function(){
@@ -178,14 +362,14 @@ page.ctrl('checkAudit', function($scope) {
 	* 加载页面模板
 	*/
 	$console.load(router.template('iframe/checkAudit'), function() {
-//		$scope.def.tabTmpl = $console.find('#checkResultTabsTmpl').html();
+		$scope.def.tabTmpl = $console.find('#checkResultTabsTmpl').html();
 		$scope.def.listTmpl = $console.find('#eleChecktmpl').html();
 //		$scope.def.selectOpttmpl = $console.find('#selectOpttmpl').html();
 		$scope.$el = {
-//			$tab: $console.find('#checkTabs'),
+			$tab: $console.find('#checkTabs'),
 			$tbl: $console.find('#eleCheck')
 		}
-//		loadLoanList(apiParams);
+		loadLoanList(apiParams);
 	})
 });
 
