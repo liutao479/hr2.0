9ca97dc91e164038142625150899a32bdcff03ec
LiuDongFeng <863258996@qq.com>
2017-03-14 17:13:50 +0800

    dropdown 默认选中项

--- src/static/js/core/core.js
+++ src/static/js/core/core.js
@@ -42,11 +42,11 @@
 			switch (name) {
 				// 周宜俭ip
 				case 'zyj':
-					return 'http://192.168.1.116:8080/' + method;
+					return 'http://192.168.1.108:8080/' + method;
 					break;
 				// 蔡延军ip
 				case 'cyj':
-					return 'http://192.168.1.116:8080/' + method;
+					return 'http://192.168.1.108:8080/' + method;
 					break;
 				// 季本松ip
 				case 'jbs':
