8b342869c59598817ddc6b845d24329ace5f1ee6
LiuDongFeng <863258996@qq.com>
2017-04-19 15:00:46 +0800

    12

--- src/static/js/core/reg.js
+++ src/static/js/core/reg.js
@@ -7,7 +7,7 @@
 	*/
 	g.regMap = {
 		userName: /^[\u4E00-\u9FA5]{1,15}$/,
-		accountName: /(^[\u4e00-\u9fa5]{1}[\u4e00-\u9fa5\.·。]{0,8}[\u4e00-\u9fa5]{1}$)|(^[a-zA-Z]{1}[a-zA-Z\s]{0,8}[a-zA-Z]{1}$)/,
+		accountName: /^[\u4E00-\u9FA5]{1,10}$/,
 		idCard: /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/,
 		accountBankName: /^[\u4e00-\u9fa5_a-zA-Z0-9]+$/,
 		bankName: /^[\u4e00-\u9fa5_a-zA-Z0-9]+$/,
