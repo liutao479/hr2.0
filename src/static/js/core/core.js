4fc163fca2d218b469546accf2d19f75cb0ab7f3
sjdong <sjdong@live.com>
2017-04-21 00:34:24 +0800

    个人中心

--- src/static/js/core/core.js
+++ src/static/js/core/core.js
@@ -43,10 +43,12 @@
 		else
 			switch (name) {
 				case 'operations':
-					return 'http://192.168.0.187:8090/' + method;
+					return 'http://192.168.0.33:8080/' + method;
+				case 'test':
+					return 'http://192.168.0.33:8080/' + method;
 				default:
 					// return 'http://192.168.1.86:8089/' + method;
-					return 'http://192.168.0.187:9999/' + method;
+					return 'http://192.168.0.33:8080/' + method;
 					// return 'http://192.168.0.186:9999/' + method;
 					// return 'http://192.168.1.194:8686/' + method;//cyj
 					// return 'http://192.168.1.74:8080/' + method;
